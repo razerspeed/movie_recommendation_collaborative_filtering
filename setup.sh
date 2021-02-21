42242160c0025c27bc22d8c508168498803d98b0
rohit kumar <45831610+razerspeed@users.noreply.github.com>
2021-02-21 09:13:47 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,35 @@
-mkdir -p ~/.streamlit/
+FROM python:3.7
 
-echo "\
+RUN pip install virtualenv
+ENV VIRTUAL_ENV=/venv
+RUN virtualenv venv -p python3
+ENV PATH="VIRTUAL_ENV/bin:$PATH"
+
+WORKDIR /app
+ADD . /app
+
+# Install dependencies
+RUN pip install -r requirements.txt
+
+# copying all files over
+COPY . /app
+
+# Expose port 
+ENV PORT 8080
+
+# cmd to launch app when container is run
+CMD streamlit run app.py
+
+# streamlit-specific commands for config
+ENV LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
+RUN mkdir -p /root/.streamlit
+RUN bash -c 'echo -e "\
+[general]\n\
+email = \"\"\n\
+" > /root/.streamlit/credentials.toml'
+
+RUN bash -c 'echo -e "\
 [server]\n\
-headless = true\n\
-port = $PORT\n\
 enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+" > /root/.streamlit/config.toml'

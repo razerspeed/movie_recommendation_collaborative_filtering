62c9afe9db90311deb47ecac07b8b43df157c2e9
rohit kumar <45831610+razerspeed@users.noreply.github.com>
2021-02-21 11:06:02 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
-email = \"razerspeed.rohit@gmail.com.com\"\n\
+email = \"razerspeed.rohit@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
-
-
